c00a353af9f33d7fead9247b4b1fa8f8f5266218
Abhinav2065 <abhinavsl511@gmail.com>
2025-10-08 02:30:53 +0545

    Added an impact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
 import Hero from './components/Hero'
-import Impact from './components/Impact'
-import Transparency from './components/Transparency'
-import Programs from './components/Programs'
-import Events from './components/Events'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import CoursesPage from './components/CoursesPage'
-import About from './components/About'
-import TeamPage from './components/TeamPage'
-import Partner from './components/Partner'
-import Getinvolved from './components/Getinvolved'
-import Donate from './components/Donate'
-import CorporatePartnerships from './components/CorporatePartnerships'
-import ContactPage from './components/ContactPage'
-import ScrollToTop from './components/ScroolToTop'
 import Navbar from './components/Navbar'
+import Impact from './components/Impact'
 import "./index.css"
 
 function App() {
@@ -30,34 +15,14 @@ function App() {
             <Route path="/" element={
               <>
               <Hero/>
+              <Impact/>
               </>
             }>
 
             </Route>
 
         </Routes>
-        {/* <ScrollToTop />
-        <Routes>
-          <Route path="/" element={
-            <>
-              <Hero />
-              <Impact />
-              <Transparency />
-              <Programs />
-              <Events />
-              <Contact />
-            </>
-          } />
-          <Route path="/courses" element={<CoursesPage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/team" element={<TeamPage />} />
-          <Route path="/partner" element={<Partner />}></Route>
-          <Route path="/get-involved" element={<Getinvolved />}> </Route>
-          <Route path="/donate" element={<Donate/>}></Route>
-          <Route path="/become-partner" element={<CorporatePartnerships/>}></Route>
-          <Route path="/contact" element={<ContactPage/>}></Route>
-        </Routes>
-        <Footer /> */}
+
       </div>
     </Router>
   )
