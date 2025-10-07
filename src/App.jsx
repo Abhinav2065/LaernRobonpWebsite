eb6a02760e49cc580a1af185608ad72656c8680c
Abhinav2065 <abhinavsl511@gmail.com>
2025-10-08 02:46:11 +0545

    Transparency section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Impact from './components/Impact'
+import Transparency from './components/Transparency'
 import "./index.css"
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
               <>
               <Hero/>
               <Impact/>
+              <Transparency/>
               </>
             }>
 
---
+++ src/components/Transparency.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../style/Transparency.css"
+
+const Transparency = () => {
+  return (
+    <div>
+        <div className="trans">
+            <div className="text">
+                <h3>We are Financially Transparent!</h3>
+                <p>Each and Every one of our financial transaction is out there acessable for everyone to see. We believe in full transparency.</p>
+                <div className="button">Check it Yourself!</div>
+            </div>
+            <div className="image">
+                <img src="transparency.png" alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Transparency
