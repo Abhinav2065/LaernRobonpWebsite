7f737c7134fd558c483be6218b8df6024024c0f6
Abhinav2065 <abhinavsl511@gmail.com>
2025-10-08 12:18:09 +0545

    Made the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import "../style/Footer.css"
+
 
 const Footer = () => {
   return (
     <div>
         <div className="footer">
-            
+            <div className="first">
+            <img src="dark-logo.png" alt="" className="logo"/>
+            <p>Learn Robo Nepal is a non profit initiative in Nepal that works on providing free education to students in Nepal.</p>
+            </div>            
+            <ul>
+                <li>
+                    <ul className='list'>
+                        <li>
+                            <h4>Home</h4>
+                        </li>
+                        <li>
+                            Impact
+                        </li>
+                        <li>
+                            Events
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <h4>About</h4>
+                </li>
+                <li>
+                    <h4>Team</h4>
+                </li>
+                <li>
+                    <h4>Contact</h4>
+                </li>
+                <li>
+                    <h4>Join Us</h4>
+                </li> 
+            </ul>
+
+            <div className="donate">
+                Donate Us
+            </div>
         </div>
     </div>
   )
